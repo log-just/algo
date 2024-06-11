44a0831b336fe9499f200750cbf9f7a3de747cb1
log <cuhaha1203@gmail.com>
2024-06-12 03:29:50 +0900

    hmm

--- src/userSolution.java
+++ src/userSolution.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 class UserSolution {
     public final static int N = 4;
@@ -9,18 +10,24 @@ class UserSolution {
         // The array of guess[] is a return array that
         // is your guess for what digits[] would be.
 
+//        List<String>[] arrayOfList = new List[2];
+        ArrayList<Integer>[] impossiable = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            impossiable[i] = new ArrayList<Integer>();
+        }
 
-//        int[][] possiable = new int[4][10];
-//        int[][] impossiable = new int[4][10];
-//
-//        Solution.Result result = Solution.query(guess);
-//        int possibleCount = result.ball+result.strike;
-//        if (possibleCount >= N) {
-////            result.strike
-//            // 모두 ball
-//        } else {
-//            // 수는 다 찾았다
-//
-//        }
+        Solution.Result result = Solution.query(guess);
+        int possibleCount = result.ball+result.strike;
+        if (possibleCount >= N) {
+            if(result.strike < 1) {
+                impossiable[0].add(2);
+            } else {
+                impossiable[0].add(2);
+            }
+            // 모두 ball
+        } else {
+            // 수는 다 찾았다
+
+        }
     }
 }
