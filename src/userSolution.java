e1ba0e2e4f185ac8df7f954e3e50e21f135fa6ea
log <cuhaha1203@gmail.com>
2024-06-12 03:10:24 +0900

    first commit

---
+++ src/userSolution.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+class UserSolution {
+    public final static int N = 4;
+
+    public void doUserImplementation(int guess[]) {
+        // Implement a user's implementation function
+        //
+        // The array of guess[] is a return array that
+        // is your guess for what digits[] would be.
+
+
+//        int[][] possiable = new int[4][10];
+//        int[][] impossiable = new int[4][10];
+//
+//        Solution.Result result = Solution.query(guess);
+//        int possibleCount = result.ball+result.strike;
+//        if (possibleCount >= N) {
+////            result.strike
+//            // 모두 ball
+//        } else {
+//            // 수는 다 찾았다
+//
+//        }
+    }
+}
